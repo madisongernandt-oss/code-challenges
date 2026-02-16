c50922a854e0acabdffbcdf16072835aeb709799
madisongernandt-oss <madison_gernandt@mymail.rcbc.edu>
2026-02-16 13:50:45 -0500

    Update Solution.cpp

--- DSA/fundamentals/arrays_and_lists/part0/cpp/Solution.cpp
+++ DSA/fundamentals/arrays_and_lists/part0/cpp/Solution.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void incrementArray(vector<int>& a, int k) {
-        
+        for (int& element : a) {
+            element += k;
+        }
     }
 };
+int main() {
+    std::vector<int> a = {1, 2, 3, 4, 5};
+    int k = 1;
+
+    Solution solution;
+    solution.incrementArray(a, k);
+
+    std::cout << "[";
+    for (size_t i = 0; i < a.size(); ++i) {
+        std::cout << a[i];
+        if (i < a.size() -1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    return 0;
+}
